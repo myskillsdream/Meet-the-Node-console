d6edd4a3745f8073c2adec6ece48733b3f18c6ac
myskillsdream <myskillsdream@gmail.com>
2022-07-16 20:46:55 +0100

    added middleware data to MyApp.js

--- myApp.js
+++ myApp.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   });
 
   function getCurrentDateString(){
-    return new Date().getUTCDate().toString();
+    return new Date().toLocaleTimeString();
   }
 
   app.get("/now", (req, res, next) => {
